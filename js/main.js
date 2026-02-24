9d91261a0e8a7ea5e6e643f0c94b5c6682fd822c
ItsWitchDoctor <arjunrao@andrew.cmu.edu>
2026-02-24 17:48:23 -0500

    Added hover effects when user hovers over a button - now scales and has some responsiveness to being clicked.

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ function makeSquaresUsingHTMLButtons(trial) {
             // Add the square's ID as the text of the button
             button.innerText = "" + squareID; // the empty string ("") is added to the squareID to convert it from a number (as it is stored in the squareData) to a string (which is what is needed for an innerText property). This is not strictly necessary in plain JavaScript -- JS will do the conversion implicitly -- but TypeScript does care, and I find it helpful to my own understanding/debugging to be careful about this kind of thing.
             button.style.fontSize = "50px"; // make the text bigger, so it's easier to read on the buttons (adjusting for larger button size)
-            // style the button to have the square's color as its background color.
+            // style the button to have the square's color as its background color. Helps the user recognize it from the indicator grid.
             button.style.background = squareColor;
             button.style.width = "165px"; // set the button to a standard width and height, for a more standardized game experience.
             button.style.height = "165px";
