9755997b26c98524c57677f4a10aaae8455d3ead
ItsWitchDoctor <arjunrao@andrew.cmu.edu>
2026-02-24 17:44:17 -0500

    Added hover effects when user hovers over a button - now scales and has some responsiveness to being clicked.

--- js/main.js
+++ js/main.js
@@ -17,8 +17,6 @@ window.addEventListener("load", function (e) {
     // Draw your clickable squares/buttons somehow. Your elements should, when clicked, invoke the trial.submitClick method, with their numerical ID as the argument: if I click button 1, it should call `trial.submitClick(1)` 
     // Try un-commenting each of these (one at a time) to see how they work.
     makeSquaresUsingHTMLButtons(trial);
-    // makeSquaresUsingCanvasAPI(trial);	
-    // makeSquaresUsingSVG(trial);	// if you un-comment this one, you also need to un-comment the line `<script src="./js/svg.js"></script>` near the end of the index.html (this is the only change you are allowed to make to the index.htm)
 });
 // =============================================================
 // ========== How to make a grid of clickable elements =========
@@ -57,16 +55,49 @@ function makeSquaresUsingHTMLButtons(trial) {
             var squareColor = squares[rowNumber][columnNumber].color;
             // Make a button element
             var button = document.createElement("button");
+            button.classList.add("hover-light");
             // Add the square's ID as the text of the button
             button.innerText = "" + squareID; // the empty string ("") is added to the squareID to convert it from a number (as it is stored in the squareData) to a string (which is what is needed for an innerText property). This is not strictly necessary in plain JavaScript -- JS will do the conversion implicitly -- but TypeScript does care, and I find it helpful to my own understanding/debugging to be careful about this kind of thing.
+            button.style.fontSize = "50px"; // make the text bigger, so it's easier to read on the buttons (adjusting for larger button size)
             // style the button to have the square's color as its background color.
-            button.style = "background: " + squareColor + ";";
+            button.style.background = squareColor;
+            button.style.width = "165px"; // set the button to a standard width and height, for a more standardized game experience.
+            button.style.height = "165px";
+            button.style.margin = "5px"; // add some space between the buttons, so they don't look like one big mass of color.
+            row.style.display = "flex"; // this makes the buttons in this row line up horizontally instead of vertically.x
+            button.style.cursor = "pointer"; // this makes the mouse cursor change to a hand when hovering over the button, which lets the user know the button is clickable.
+            button.style.transition = "transform 50ms linear"; // this makes it so that when we apply a transform to a button, like in our size scaling hover effects below, the animation isn't super abrupt, and takes a bit of time to render.
+            button.style.transformOrigin = "center center"; // this makes the button grow/shrink from its center when we apply the transform in the event listeners below - helps make it visually clearer
             // Very important: we need to be able to tell the trial engine when this button has been clicked! Since we are making these as their own HTML elements, we can add a click listener to each. The handler will report the click to the trial engine using the trial.submitClick method. The handler function is being defined in-place (anonymously) right in the addEventListener method call.
             button.addEventListener("click", function () {
                 // Depending on your programming background (which language[s] you are more familiar with), you may be suspicious about using the "squareID" variable in this click handler function, since you may have noticed that it is only declared within this inner loop and its value will be different each time through the loop.
                 // However, *will* work in JS, using a language feature called a "closure": because the variable exists with a value at the time that the function is defined (right here, within this instance of the per-square loop), it will continuing existing within that function even if alternate-universe versions of it are created the other times through the loop. MDN's explanation (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures) is a little long/confusing IMO but here's a tiktok: https://www.tiktok.com/@snack.js/video/7606405733172694292
                 // P.S. One of the "subtle differences between var and let" that I mentioned in class is how they work with closures -- see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures#creating_closures_in_loops_a_common_mistake for details.
                 trial.submitClick(squareID);
+            });
+            // this event listener makes the button grow a little when the mouse hovers over it (scaled by a factor of 1.06)
+            button.addEventListener("mouseenter", function () {
+                // This makes the button scale up by a factor of 1.06.
+                button.style.transform = "scale(1.06)";
+                // zIndex here makes it so that the hovered button will be on TOP of other buttons, so it won't get 'cut off' by other buttons
+                button.style.zIndex = "10";
+            });
+            // this event listener makes the button return to its normal size when the mouse leaves it
+            button.addEventListener("mouseleave", function () {
+                // this returns the button to its normal size after mouse has left its surface area
+                button.style.transform = "scale(1.0)";
+                // this resets the button back to its normal zIndex, so it won't be on top of other buttons when its not being hovered over
+                button.style.zIndex = "1";
+            });
+            // this is a little bit of styling to make the button look like its being pressed when its clicked
+            // it provides active response to the user, which was some of our feedback
+            button.addEventListener("mousedown", function () {
+                button.style.transform = "scale(0.98)";
+            });
+            // this makes the button return to its hover size when the mouse button is released
+            // so it won't permanently look like it's being pressed after one click
+            button.addEventListener("mouseup", function () {
+                button.style.transform = "scale(1.06)";
             });
             // then, add this button to the row
             row.appendChild(button);
@@ -77,105 +108,6 @@ function makeSquaresUsingHTMLButtons(trial) {
         }
         // Now that this row has its buttons in it, add it to the grid. (We could actually have added it *before* filling it up -- appendChild would work either way -- but I use this approach to building up 2D grids with other data types as well, and there are several different kinds of mutability in JS.)
         grid.appendChild(row);
-    }
-}
-// Here is a version using the Canvas API. "canvas" is a built-in HTML element type that has a special set of features such that you can draw raster images directly into it using functions like "fillRect". If you are familiar with Processing, you will find that it has very similar capabilities. Here's the main MDN documentation page:
-// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-// ...and I do recommend their tutorial:
-// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
-function makeSquaresUsingCanvasAPI(trial) {
-    console.log("Canvas variant!");
-    // Just as in the above version of the grid-drawing function, you'll need the squares data and the mainDiv.
-    var squares = trial.getSquaresData();
-    var mainDiv = document.getElementById("main");
-    // This time, make a *canvas* element
-    var canvasElement = document.createElement("canvas");
-    // And, as before, set it to the Bakeoff rules max size and append it to the mainDiv.
-    canvasElement.width = canvasSize;
-    canvasElement.height = canvasSize;
-    mainDiv.appendChild(canvasElement);
-    // Somewhat confusingly, we don't address Canvas API calls to the canvas element itself. We need a "rendering context," which we ask the canvas for. (There are several different ones -- we're going to use the "2d" one, for drawing shaps/pixels in 2D.) All the drawing methods belong to this "draw" variable, not the underlying canvas element.
-    var draw = canvasElement.getContext("2d");
-    // Some consts to change where the squares are drawn.
-    var margin = 200; // the margin around the squares
-    var padding = 50; // padding between squares
-    var squareSize = 40; // their width/height
-    // As in the above version, we use a nested loop to go through the 2D array of squares data.
-    for (var rowNumber = 0; rowNumber < squares.length; rowNumber++) {
-        for (var columnNumber = 0; columnNumber < squares[rowNumber].length; columnNumber++) {
-            // Calculate where this square should be, based on the padding and margins:
-            var x = (columnNumber * (padding + squareSize)) + margin;
-            var y = (rowNumber * (padding + squareSize)) + margin;
-            // get the id and color data for this square
-            var squareID = squares[rowNumber][columnNumber].id;
-            var squareColor = squares[rowNumber][columnNumber].color;
-            // set the fill color to the color of this square
-            draw.fillStyle = squareColor;
-            // and draw a rectangle here
-            draw.fillRect(x, y, squareSize, squareSize);
-            // add a text label of this square's ID
-            draw.fillStyle = "#000"; // black, to draw the text
-            draw.fillText("" + squareID, x, y);
-        }
-    }
-    // The major drawback of using pure raw pixels is that our "buttons" are actually just rectangles drawn on the page -- they don't have any interactive ability of their own. So to know when a rectangle is clicked, we need to listen for a click *anywhere* on the canvas, then figure out which (if any) rectangle is at that place on the canvas.
-    canvasElement.addEventListener("click", function (e) {
-        // First, get the x and y locations of the click from the event object that was passed into this handler by the listener.
-        // Pointer events (like click and move) give you several different x and y options. The "offsetX" and "offsetY" variants are the ones that tell you the coordinates relative to the bounding box of the element (so, the same coordinate system as we used while drawing).
-        var x = e.offsetX;
-        var y = e.offsetY;
-        // To get the row/column number, have to reverse the math of how we drew the squares in the first place...
-        var rowNumber = Math.floor((y - margin) / (padding + squareSize));
-        var columnNumber = Math.floor((x - margin) / (padding + squareSize));
-        // ...and then we use those to look up the id number in the squares data.
-        if (rowNumber >= 0 && rowNumber < squares.length && columnNumber >= 0 && columnNumber < squares[0].length) { // make sure there's a square here	
-            var squareID = squares[rowNumber][columnNumber].id;
-            // console.log(squareID);
-            // and submit it to the trial judge!
-            trial.submitClick(squareID);
-        }
-        else {
-            // trial.submitClick(-1);
-        }
-    });
-}
-// Here is a version using the svg.js library to produce an svg (vector) display. Like the HTML button variant and unlike the Canvas variant, you can attach an event listener to an SVG element. Unlike the HTML button variant and more like the Canvas variant, you have a lot more control over the exact shape/position of the clickable elements. The drawback is that SVG has some quirks. I've found that the svg.js library makes many of these less bad, though it does introduce some of its own.
-// I've included the svg.js in this directory for portability, but it was *not written by me*. The documentation is at: https://svgjs.dev/docs/3.0/
-// If you want to use the SVG library, you *must* uncomment the line `<script src="./js/svg.js"></script>` near the end of the index.html (this is the only change you are allowed to make to the index.htm).
-function makeSquaresUsingSVG(trial) {
-    console.log("SVG variant!");
-    // As before, get the squares data.
-    var squares = trial.getSquaresData();
-    // Some consts to change where the squares are drawn.
-    var margin = 200; // the margin around the squares
-    var padding = 50; // padding between squares
-    var squareSize = 40; // their width/height
-    // SVG.js will handling making the necessary new SVG container element and setting its size; you just have to tell it what parent element to add to. In this case, the # in "#main" means "id='main'" -- this is a syntax shorthand that many JS frameworks use.
-    var svg = SVG().addTo('#main').size(canvasSize, canvasSize);
-    // As before -- a nested loop to go through the provided squares data.
-    for (var rowNumber = 0; rowNumber < squares.length; rowNumber++) {
-        var _loop_2 = function (columnNumber) {
-            // Calculate where this square should be:
-            var x = (columnNumber) * (padding + squareSize) + margin;
-            var y = Math.floor(rowNumber) * (padding + squareSize) + margin;
-            // get the id and color data for this square
-            var squareID = squares[rowNumber][columnNumber].id;
-            var squareColor = squares[rowNumber][columnNumber].color;
-            // In svg.js, instead of drawing a rectangle "at" a location, "with" a particular color, we first make a rectangle at a particular size...
-            var square = svg.rect(squareSize, squareSize);
-            // ...and then we move it to where we want it. (This is different from Processing or the Canvas API, where we make them "in the right place" to start with.)
-            square.move(x, y);
-            // and we can change the element's color at any time after it exists:
-            square.fill(squareColor);
-            // (see the svg.js documentation for more information on colors, fill vs stroke, etc)
-            // SVG.js has a slightly different way of doing event listeners. For the click event, it looks like this:
-            square.click(function () {
-                trial.submitClick(squareID);
-            });
-        };
-        for (var columnNumber = 0; columnNumber < squares[rowNumber].length; columnNumber++) {
-            _loop_2(columnNumber);
-        }
     }
 }
 //# sourceMappingURL=main.js.map
